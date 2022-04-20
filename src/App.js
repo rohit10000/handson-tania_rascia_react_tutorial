f6df949eb4b998ea9f72c2a18ddc07de2e209215
Rohit Singh <rohitsingh18997@gmail.com>
2022-04-20 22:32:56 +0530

    state in react: state change using form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
     state = {
-        characters: [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-            },
-            {
-                name: 'Mac',
-                job: 'Bouncer',
-            },
-            {
-                name: 'Dee',
-                job: 'Aspring actress',
-            },
-            {
-                name: 'Dennis',
-                job: 'Bartender',
-            },
-        ],
+        characters: [],
     }
     removeCharacter = (index) => {
         const { characters } = this.state
@@ -31,12 +15,18 @@ class App extends Component {
             }),
         })
     }
+
+    handleSubmit = (character) => {
+        this.setState({ characters: [...this.state.characters, character] })
+    }
+
     render() {
         const { characters } = this.state
 
         return (
             <div className="container">
-                <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
+                <Form handleSubmit={this.handleSubmit}/>
             </div>
         )
     }
